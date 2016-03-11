bd2ba888843b8c359452f47a6d3befac2714a914
jmedley@google.com <jmedley@google.com>
2016-03-10 19:30:12 -0800

    Add styling and a fetch event to the useless app.

--- sw.js
+++ sw.js
@@ -1,40 +1,52 @@
+"use strict";
+
 var MESSAGES = ['Are we there yet?',
-				'I&#39;m tired.',
-				'Look out!',
-				'Is it safe?',
-				'MOAR',
-				'Watson! Come here.',
-				'Wah!',
-				'Nooooooooo!',
-				'Help!',
-				'I thought turkies could fly.'];
+        'I&#39;m tired.',
+        'Look out!',
+        'Is it safe?',
+        'MOAR',
+        'Watson! Come here.',
+        'Wah!',
+        'Nooooooooo!',
+        'Help!',
+        'I thought turkies could fly.'];
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  if (e.request.url.indexOf('useful1.html') > 0) {
+    var res = new Response("You really thought that would work? Really?!");
+    e.respondWith(res);
+  } else {
+    return fetch(e.request);
+  };
+});
 
 self.addEventListener('push', function(e) {
-	var index = Math.floor(Math.random() * 9);
-	var options = {
-     	body: MESSAGES[index]
-  	};
-  	var title = "Useless App Needs You NOW!";
+  var index = Math.floor(Math.random() * 9);
+  data = {
+    body: MESSAGES[index]
+  };
+  var title = "Useless App Needs You NOW!";
 
-	e.waitUntil(
-    	self.registration.showNotification(title, options)
- 	);
+  e.waitUntil(
+    self.registration.showNotification(title, data)
+  );
 });
 
 self.addEventListener('notificationclick', function(event) {
-  	event.notification.close();
+    event.notification.close();
 
-  	event.waitUntil(clients.matchAll({
-  		type: 'window'
-  	}).then(function(clientList) {
-    	for (var i = 0; i < clientList.length; i++) {
-     		var client = clientList[i];
-     	if (client.url === '/' && 'focus' in client) {
-        	return client.focus();
-     	}
+    event.waitUntil(clients.matchAll({
+      type: 'window'
+    }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+      if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+      }
     }
     if (clients.openWindow) {
-    	return clients.openWindow('/useless.html');
+      return clients.openWindow('/useless.html');
     }
   }));
 });
