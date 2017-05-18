b349bb3d3cd6459862a49b97223cee519818d9f8
Joseph Medley <jmedley@google.com>
2017-05-18 16:02:51 -0700

    Build with workbox-build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,18 @@
 'use strict';
 
-var gulp = require('gulp');
+const gulp = require('gulp');
+const build = require('workbox-build');
+
+gulp.task('bundle', () => {
+   return build.generateSW({
+     globDirectory: './app/',
+     swDest: './app/sw.js',
+     staticFileGlobs: ['**\/*.{html,js,css}']
+   })
+   .then(() => {
+     console.log('Service worker generated.');
+   })
+   .catch((err) => {
+     console.log('[ERROR] This happened: ' + err);
+   });
+})
